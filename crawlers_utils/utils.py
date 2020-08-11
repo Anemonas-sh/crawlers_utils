5fd43eb585386553b667c77607b81b18a15b49ef
Lucas Raggi <lrr@ic.ufal.br>
2020-08-10 21:18:54 -0300

    package repo

--- crawlers_utils/utils.py
+++ crawlers_utils/utils.py
@@ -5,8 +5,13 @@ from googleapiclient import discovery
 from google.cloud import storage
 
 
-def upload_folder_to_bucket(bucket_name, local_path, bucket_path, recursive_upload=True):
-    bucket = bucket_name
+def connect_to_storage(bucket_name):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    return bucket
+
+
+def upload_folder_to_bucket(bucket, local_path, bucket_path, recursive_upload=True):
     for local_file in glob.glob(local_path + '/**'):
         if not os.path.isfile(local_file) and recursive_upload:
             upload_folder_to_bucket(bucket, local_file, bucket_path + "/" + os.path.basename(local_file))
@@ -17,9 +22,3 @@ def upload_folder_to_bucket(bucket_name, local_path, bucket_path, recursive_uplo
             blob = bucket.blob(remote_path)
             print('sent {}'.format(local_file.split("/")[-1]))
             blob.upload_from_filename(local_file)
-
-
-def connect_to_storage(bucket_name):
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    return bucket
