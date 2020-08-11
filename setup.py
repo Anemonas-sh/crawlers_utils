0cc7ae431af71ae62484879cb460707947d91cef
Lucas Raggi <lrr@ic.ufal.br>
2020-08-10 21:11:53 -0300

    package repo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crawlers_utils',
+    version='1.0.0',
+    description='Datatour crawlers utils library',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'google-api-python-client==1.8.3',
+        'google-cloud-storage==1.29.0',
+    ],
+)
