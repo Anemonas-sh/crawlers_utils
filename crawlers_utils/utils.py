4eebcdd1cb8b8359b2c7ab8341ac05308a71c7bd
Lucas Raggi <lrr@ic.ufal.br>
2020-08-23 13:05:32 -0300

    add save_file

--- crawlers_utils/utils.py
+++ crawlers_utils/utils.py
@@ -1,8 +1,16 @@
 import os
 import glob
 import posixpath
+import json
 from googleapiclient import discovery
 from google.cloud import storage
+
+
+def save_file(file_path, data, bucket=None):
+    with open(file_path, "w", encoding="utf-8") as f:
+        print(json.dumps(data, ensure_ascii=False), file=f)
+    if bucket is not None:
+        upload_folder_to_bucket(bucket, file_path, file_path)
 
 
 def connect_to_storage(bucket_name):
@@ -11,14 +19,7 @@ def connect_to_storage(bucket_name):
     return bucket
 
 
-def upload_folder_to_bucket(bucket, local_path, bucket_path, recursive_upload=True):
-    for local_file in glob.glob(local_path + '/**'):
-        if not os.path.isfile(local_file) and recursive_upload:
-            upload_folder_to_bucket(bucket, local_file, bucket_path + "/" + os.path.basename(local_file))
-        else:
-            if '.' not in local_file:
-                continue
-            remote_path = posixpath.join(bucket_path, local_file[1 + len(local_path):])
-            blob = bucket.blob(remote_path)
-            print('sent {}'.format(local_file.split("/")[-1]))
-            blob.upload_from_filename(local_file)
+def upload_folder_to_bucket(bucket, local_path, bucket_path):
+    blob = bucket.blob(bucket_path)
+    blob.upload_from_filename(local_path)
+
