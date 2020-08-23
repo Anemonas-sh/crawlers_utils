0bc182e8d7b6ba40c438c977992e92a0b7f17663
França Mac Dowell <francamacdowell@gmail.com>
2020-08-23 10:41:18 -0300

    Add download from bucket blob

--- crawlers_utils/utils.py
+++ crawlers_utils/utils.py
@@ -7,6 +7,8 @@ from shutil import make_archive
 
 def connect_to_storage(bucket_name):
     storage_client = storage.Client()
+    # Alternatively access Client from filename:
+    # storage_client = storage.Client.from_service_account_json('service_account.json')
     bucket = storage_client.get_bucket(bucket_name)
     return bucket
 
@@ -22,6 +24,31 @@ def upload_folder_to_bucket(bucket, local_path, bucket_path, recursive_upload=Tr
             blob = bucket.blob(remote_path)
             print('sent {}'.format(local_file.split("/")[-1]))
             blob.upload_from_filename(local_file)
+
+
+def download_blob_from_bucket(bucket_name, bucket_path, source_name, path_to_save):
+    """ Download blob from Storage bucket
+    
+    Parameters:
+    bucket_name (str): Storage bucket name which will access. e.g.: "toureyes-data-lake"
+    bucket_path (str): Path inside Google Storage bucket where your blob is find. e.g.:
+    source_folder_name (str): Blob name to download from full storage path
+    path_to_save (str): Full local path to download blob
+    
+    Returns:
+    Nothing
+    """
+
+    bucket = connect_to_storage(bucket_name=bucket_name)
+    blob = bucket.blob(bucket_path + "/" + source_name)
+    print("Downloading: {}".format(bucket_path + "/" + source_name))
+    blob.download_to_filename(path_to_save + "/" + source_name)
+    
+    print(
+        "Blob {} downloaded to {}.".format(
+            source_name, path_to_save
+        )
+    )
 
 
 def create_compressed_folder(output_path: str = None, filename: str = None, compression_type="zip", base_dir: str = None):
