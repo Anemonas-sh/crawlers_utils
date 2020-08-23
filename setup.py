950d0cbf4e4451518972cc17433f01eb4b82ea84
Lucas Raggi <lrr@ic.ufal.br>
2020-08-23 13:19:28 -0300

    add __init__.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0.0',
     description='Datatour crawlers utils library',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'google-api-python-client==1.8.3',
         'google-cloud-storage==1.29.0',
