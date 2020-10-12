5a9a0cdfaf4b70e6e2564ae492b966f7a7004bc3
Nelson Gomes Neto <ngomesneto@outlook.com>
2020-10-12 11:34:13 -0300

    Add pyvirtualdisplay to requirements

--- setup.py
+++ setup.py
@@ -9,5 +9,7 @@ setup(
     install_requires=[
         'google-api-python-client==1.8.3',
         'google-cloud-storage==1.29.0',
+        'pyvirtualdisplay==1.3.2',
+        'selenium==3.141.0'
     ],
 )
